ecfc8661131f8a3a511811fc0c71ac3b3e112bdc
Gustavo Osan <gustavoosan@gmail.com>
2025-07-05 23:06:23 -0300

    primera carga

---
+++ index.js
@@ -0,0 +1,8 @@
+let variableA="Hello";
+console.log(variableA);
+variableA = "World";
+console.log(variableA);
+variableA = 5;
+console.log(variableA);
+variableA = [1,2,3];
+console.log(variableA);
