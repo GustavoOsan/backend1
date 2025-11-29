fe63296117848ac2bb218a05050615376a000a01
Gustavo Osan <gustavoosan@gmail.com>
2025-11-29 16:50:53 -0300

    Entrega Final Backend 1 correccion .env

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const { Server } = require('socket.io');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
+
 const productsRouter = require('./routes/products.router.js');
 const cartsRouter = require('./routes/carts.router.js');
 const viewsRouter = require('./routes/views.router.js');
-
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://localhost:27017/ecommerce')
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("Conectado a la base de datos 'ecommerce' correctamente");
     })
@@ -18,14 +19,12 @@ mongoose.connect('mongodb://localhost:27017/ecommerce')
         console.error("Error al conectar a la base de datos:", error);
     });
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', handlebars.engine());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
-
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
@@ -37,7 +36,6 @@ app.use((req, res, next) => {
     req.context = { socketServer };
     next();
 });
-
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
